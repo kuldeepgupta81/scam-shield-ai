b80db5121d19c470910be74e803daab0936f17d1
Kuldeep Pratap <811252kuldeep@gmail.com>
2026-03-01 18:49:37 +0530

    final working build 🚀

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  turbopack: {
-    root: "C:/Users/HP/OneDrive/Desktop/my-project/scam-detector-ai",
-  },
-};
-
-export default nextConfig;
